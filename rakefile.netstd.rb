fa774c6d42d9cafc5f53b1c4e2d1ea0b2c011ee4
Blair Conrad <blair@blairconrad.com>
2016-10-03 06:59:22 -0400

    add default cacert.pem

--- rakefile.netstd.rb
+++ rakefile.netstd.rb
@@ -50,7 +50,7 @@ approval_tests = [
   "tests/FakeItEasy.Tests.Approval.netstd/bin/Release/FakeItEasy.Tests.Approval.netstd.dll"
 ]
 
-repo = 'FakeItEasy/FakeItEasy'
+repo = 'blairconrad/FakeItEasy'
 appveyor_server_url = 'https://ci.appveyor.com/project/FakeItEasy/fakeiteasy/settings'
 release_issue_labels = ['P2', 'build', 'documentation']
 release_issue_common_steps = <<-eos
@@ -93,7 +93,7 @@ release_body = <<-eos
 * _&lt;user's actual name&gt;_ - @_&lt;github userid&gt;_
 eos
 
-ssl_cert_file_url = "http://curl.haxx.se/ca/cacert.pem"
+ssl_cert_filename = "cacert.pem"
 
 Albacore.configure do |config|
   config.log_level = :verbose
@@ -135,15 +135,15 @@ task :next_version, :new_version do |asm, args|
 
   current_branch = `git rev-parse --abbrev-ref HEAD`.strip()
 
-  if current_branch != 'master'
-    fail "ERROR: Current branch is '#{current_branch}'. Must be on branch 'master' to set new version."
-  end if
+  #if current_branch != 'master'
+  #  fail "ERROR: Current branch is '#{current_branch}'. Must be on branch 'master' to set new version."
+  #end if
 
   new_branch = "set-version-to-" + new_version
 
   require 'octokit'
 
-  ssl_cert_file = get_temp_ssl_cert_file(ssl_cert_file_url)
+  find_ssl_cert_file(ssl_cert_filename)
 
   client = Octokit::Client.new(:netrc => true)
 
@@ -198,7 +198,7 @@ task :pre_release, :version_suffix do |asm, args|
 
   require 'octokit'
 
-  ssl_cert_file = get_temp_ssl_cert_file(ssl_cert_file_url)
+  find_ssl_cert_file(ssl_cert_filename)
 
   client = Octokit::Client.new(:netrc => true)
 
@@ -378,30 +378,26 @@ def restore_dependencies_for_project(project_dir)
   restore_cmd.execute
 end
 
-# Get a temporary SSL cert file if necessary.
-# If ENV["SSL_CERT_FILE"] is set, will return nil.
-# Otherwise, attempts to download a known
-# SSL cert file, sets ENV["SSL_CERT_FILE"]
-# to point at it, and returns the file (mostly so it will
-# stay in scope while it's needed).
-def get_temp_ssl_cert_file(ssl_cert_file_url)
+# Find a  SSL cert file if necessary.
+# If ENV["SSL_CERT_FILE"] is set, takes no action.
+# Otherwise, expects the current directory to
+# contain a "cacert.pem" file that can be used
+# to enable Rusy SSL support, and sets
+# ENV["SSL_CERT_FILE"] to point at it.
+# New cert files can be had from
+#    http://curl.haxx.se/ca/cacert.pem
+# in case the current one stops working.
+def find_ssl_cert_file(default_ssl_cert_filename)
   ssl_cert_file_path = ENV["SSL_CERT_FILE"]
   if ssl_cert_file_path
-    return nil
+    puts "Using SSL cert file from environment variable SSL_CERT_FILE"
+  else
+    puts "Using standard SSL cert file #{default_ssl_cert_filename}"
+    ssl_cert_file_path = File.absolute_path(default_ssl_cert_filename)
+    ENV["SSL_CERT_FILE"] = ssl_cert_file_path
   end
 
-  puts "Environment variable SSL_CERT_FILE is not set. Downloading a cert file from '#{ssl_cert_file_url}'..."
-
-  require 'open-uri'
-  require 'tempfile'
-
-  file = Tempfile.new('ssl_cert_file')
-  file.binmode
-  file << open(ssl_cert_file_url).read
-  file.close
-
-  ENV["SSL_CERT_FILE"] = file.path
-
-  puts "Downloaded cert file to '#{ENV['SSL_CERT_FILE']}'."
-  return file
+  if ! File.exist?(ssl_cert_file_path)
+    raise "ERROR: SSL cert file #{ssl_cert_file_path} does not exist"
+  end
 end
--- rakefile.rb
+++ rakefile.rb
@@ -86,7 +86,7 @@ release_body = <<-eos
 * _&lt;user's actual name&gt;_ - @_&lt;github userid&gt;_
 eos
 
-ssl_cert_file_url = "http://curl.haxx.se/ca/cacert.pem"
+ssl_cert_filename = "cacert.pem"
 
 Albacore.configure do |config|
   config.log_level = :verbose
@@ -128,7 +128,7 @@ task :next_version, :new_version do |asm, args|
 
   require 'octokit'
 
-  ssl_cert_file = get_temp_ssl_cert_file(ssl_cert_file_url)
+  find_ssl_cert_file(ssl_cert_filename)
 
   client = Octokit::Client.new(:netrc => true)
 
@@ -183,7 +183,7 @@ task :pre_release, :version_suffix do |asm, args|
 
   require 'octokit'
 
-  ssl_cert_file = get_temp_ssl_cert_file(ssl_cert_file_url)
+  find_ssl_cert_file(ssl_cert_filename)
 
   client = Octokit::Client.new(:netrc => true)
 
@@ -344,30 +344,26 @@ def run_tests(test_assemblies, command, result_dir)
   end
 end
 
-# Get a temporary SSL cert file if necessary.
-# If ENV["SSL_CERT_FILE"] is set, will return nil.
-# Otherwise, attempts to download a known
-# SSL cert file, sets ENV["SSL_CERT_FILE"]
-# to point at it, and returns the file (mostly so it will
-# stay in scope while it's needed).
-def get_temp_ssl_cert_file(ssl_cert_file_url)
+# Find a  SSL cert file if necessary.
+# If ENV["SSL_CERT_FILE"] is set, takes no action.
+# Otherwise, expects the current directory to
+# contain a "cacert.pem" file that can be used
+# to enable Rusy SSL support, and sets
+# ENV["SSL_CERT_FILE"] to point at it.
+# New cert files can be had from
+#    http://curl.haxx.se/ca/cacert.pem
+# in case the current one stops working.
+def find_ssl_cert_file(default_ssl_cert_filename)
   ssl_cert_file_path = ENV["SSL_CERT_FILE"]
   if ssl_cert_file_path
-    return nil
+    puts "Using SSL cert file from environment variable SSL_CERT_FILE"
+  else
+    puts "Using standard SSL cert file #{default_ssl_cert_filename}"
+    ssl_cert_file_path = File.absolute_path(default_ssl_cert_filename)
+    ENV["SSL_CERT_FILE"] = ssl_cert_file_path
   end
 
-  puts "Environment variable SSL_CERT_FILE is not set. Downloading a cert file from '#{ssl_cert_file_url}'..."
-
-  require 'open-uri'
-  require 'tempfile'
-
-  file = Tempfile.new('ssl_cert_file')
-  file.binmode
-  file << open(ssl_cert_file_url).read
-  file.close
-
-  ENV["SSL_CERT_FILE"] = file.path
-
-  puts "Downloaded cert file to '#{ENV['SSL_CERT_FILE']}'."
-  return file
+  if ! File.exist?(ssl_cert_file_path)
+    raise "ERROR: SSL cert file #{ssl_cert_file_path} does not exist"
+  end
 end
